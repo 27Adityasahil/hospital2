0098584a6a774e4cf3cbbff8be3c83abb214c225
27Adityasahil <adityasahil24@gmail.com>
2025-07-03 16:58:08 +0530

    Update About

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import CommonBanner from '../components/CommonBanner'
+import aboutImg from '../assets/images1/about-img.png'
+
+import './About.css'
 
 function About() {
   return (
@@ -9,6 +12,29 @@ function About() {
         subtitle="Comprehensive healthcare services with expert medical professionals"
         backgroundImage="https://images.pexels.com/photos/4386467/pexels-photo-4386467.jpeg?auto=compress&cs=tinysrgb&w=1200"
       />
+
+      <div className="about-section">
+        <div className="left-about">
+          <img src= {aboutImg} alt="about-Bimal Hospital" />
+        </div>
+        <div className="right-about">
+          <h4>Welcome To Dr. Bimal Hospital Research Center Pvt. Ltd.</h4>
+          <p>Dr. Bimal Hospital has the most modern world class facilities for Gastroenterology, Nephrology, Obst. & Gynecology. It has One Modular Operation Theater, One minor Operation Theater & One Emergency Operation Theater.</p>
+          <p>The OT’s have World class OT Lights, Anaesthesia Ventilator, Central Suction & Central Gas Pipelines, One of the best Cautery & Vessel Sealers. Utmost planning for Operation Threater has been done for space utilisation for as this is divided in various Zones namely, Clean Zone, SemiSterile Zone, & Sterile Zone. A standard size CSSD (Sterlisation unit) fully equipped with Two Big size Horizontal Autoclaves, Flash Autoclave, ETO Sterliser, Ultrasonic Cleaner etc.</p>
+          <p>The Operation Theater is backed up with Fully Equipped ICU & Post operative ward. At present it is 100 beded facility with hitech infrastructure. Offering clinical services in the field of super specility Gastroenterology, Nephrology, Obst. & Gynecology.</p>
+
+          <div className="points">
+            <ul>
+              <li> 24/7 hours support</li>
+              <li>Emergency Service</li>
+            </ul>
+            <ul>
+              <li>99% satisfaction</li>
+              <li>Patient Centered Care</li>
+            </ul>
+          </div>
+        </div>
+      </div>
 
     </section>
   )
